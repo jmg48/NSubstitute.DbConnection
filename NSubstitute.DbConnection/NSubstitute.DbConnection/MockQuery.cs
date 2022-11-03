852189ef0c5f42481ecfa93617b79f08ad7b1f64
Jon Glass <jon.glass@eso.com>
2022-11-03 20:35:53 +0000

    Update package version

--- NSubstitute.DbConnection/NSubstitute.DbConnection/MockQuery.cs
+++ NSubstitute.DbConnection/NSubstitute.DbConnection/MockQuery.cs
@@ -72,7 +72,7 @@
         public void Affects(Func<QueryInfo, int> rowCountSelector) => RowCountSelector = rowCountSelector;
 
         public void Throws<T>(T exception)
-            where T : Exception => ResultSelectors.Add((typeof(T), _ => { throw exception; }));
+            where T : Exception => ResultSelectors.Add((typeof(T), _ => throw exception));
 
         public bool Matches(IDbCommand command)
         {
